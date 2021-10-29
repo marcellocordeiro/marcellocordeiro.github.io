9325d436fa4a184aecf78c2d70ae4685f63b5330
Marcello Cordeiro <msc3@cin.ufpe.br>
2021-10-29 20:08:22 -0300

    Base Next.js project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from "next/app";
+
+import "tailwindcss/tailwind.css";
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
