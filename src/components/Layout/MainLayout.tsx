767f18eadea061abf37123d290d00c55563f65fb
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2022-01-25 02:27:42 -0300

    Update deps and use prettier-plugin-tailwindcss

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -11,19 +11,19 @@ export const MainLayout = ({ children }: Props) => (
     <Head title="Create Next App" />
 
     <div className="py-0 px-8">
-      <main className="flex flex-1 flex-col justify-center items-center min-h-screen py-16 px-0">
+      <main className="flex min-h-screen flex-1 flex-col items-center justify-center py-16 px-0">
         {children}
       </main>
 
-      <footer className="flex flex-1 justify-center items-center py-8 px-0 border-t border-solid border-gray-200">
+      <footer className="flex flex-1 items-center justify-center border-t border-solid border-gray-200 py-8 px-0">
         <a
-          className="flex flex-grow justify-center items-center"
+          className="flex flex-grow items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <span className="h-4 ml-2">
+          <span className="ml-2 h-4">
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
