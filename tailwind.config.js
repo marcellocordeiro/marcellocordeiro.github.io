63d72f5a8830240a328b1f4ab66d70fd265d82e6
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2022-03-21 03:09:31 -0300

    Better linting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/pages/**/*.{js,tsx}", "./src/components/**/*.{js,tsx}"],
   theme: {
     extend: {},
   },
