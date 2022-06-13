e642990de699d5c70c80092cb0b00082a3db47bd
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2022-06-13 02:14:05 -0300

    Update deps

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true,
+    newNextLinkBehavior: true,
+  },
 };
 
 module.exports = nextConfig;
